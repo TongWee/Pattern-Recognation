87b86e6d9ed3e54c198023bb64ed5583a8c72159
TongWee <308992640@qq.com>
2015-06-05 10:52:44 +0800

    Add weights
    Correct:548
    Incorrect:220
    YesToNo:206   NoToYes:14
    Accuracy:71.3542%

--- src/PID_Dataset_Test.java
+++ src/PID_Dataset_Test.java
@@ -9,163 +9,255 @@ public class PID_Dataset_Test {
     {
         double p=0;
         double p1,p2,p3,p4,p5,p6,p7,p8;
-        if (num1 <= 50)
+        double w1,w2,w3,w4,w5,w6,w7,w8;
+        if (num1 <= 50){
             p1 = 0.007;
-        else if (num1 <= 100)
+            w1 = 33.333;
+        }
+        else if (num1 <= 100){
             p1 = 0.067;
-        else if (num1 <= 150)
+            w1 = 8.654;
+        }
+        else if (num1 <= 150){
             p1 = 0.534;
-        else
+            w1 = 34.541;
+        }
+        else{
             p1 = 0.392;
-
-        if (num2 <= 60)
+            w1 = 75;
+        }
+        if (num2 <= 60){
             p2 = 0.146;
-        else if (num2 <= 80)
+            w2 = 24.684;
+        }
+        else if (num2 <= 80){
             p2 = 0.567;
-        else if (num2 <= 100)
+            w2 = 34.157;
+        }
+        else if (num2 <= 100){
             p2 = 0.257;
-        else
+            w2 = 45.395;
+        }
+        else{
             p2 = 0.03;
+            w2 = 61.538;
+        }
 
-        if (num3 <= 100)
+        if (num3 <= 100){
             p3 = 0.586;
-        else if (num3 <= 200)
+            w3 = 29.9;
+        }
+        else if (num3 <= 200){
             p3 = 0.243;
-        else if (num3 <= 300)
+            w3 = 41.1;
+        }
+        else if (num3 <= 300){
             p3 = 0.093;
-        else
+            w3 = 52.1;
+        }
+        else{
             p3 = 0.078;
-
-        if (num4 < 30)
+            w3 = 56.8;
+        }
+        if (num4 < 30){
             p4 = 0.198;
-        else if (num4 < 40)
+            w4 = 17.492;
+        }
+        else if (num4 < 40){
             p4 = 0.601;
-        else
+            w4 = 43.631;
+        }
+        else{
             p4 = 0.201;
-
-        if (num5 <= 0.5)
+            w4 = 56.25;
+        }
+        if (num5 <= 0.5){
             p5 = 0.53;
-        else if (num5 <= 1)
+            w5 = 28.921;
+        }
+        else if (num5 <= 1){
             p5 = 0.362;
-        else if (num5 <= 2)
+            w5 = 42.92;
+        }
+        else if (num5 <= 2){
             p5 = 0.097;
-        else
+            w5 = 55.319;
+        }
+        else{
             p5 = 0.011;
-
-        if (num6 <= 20)
+            w5 = 75;
+        }
+        if (num6 <= 20){
             p6 = 0;
-        else if (num6 <= 30)
+            w6 = 0;
+        }
+        else if (num6 <= 30){
             p6 = 0.336;
-        else if (num6 <= 40)
+            w6 = 21.6;    
+        }
+        else if (num6 <= 40){
             p6 = 0.284;
-        else if (num6 <= 50)
+            w6 = 48.4;
+        }
+        else if (num6 <= 50){
             p6 = 0.239;
-        else if (num6 <= 60)
+            w6 = 56.6;
+        }
+        else if (num6 <= 60){
             p6 = 0.116;
-        else
+            w6 = 57.4;
+        }
+        else{
             p6 = 0.026;
-
-        if (num7 <= 4)
+            w6 = 25.9;
+        }
+        if (num7 <= 4){
             p7 = 0.507;
-        else if (num7 <= 8)
+            w7 = 27.642;
+        }
+        else if (num7 <= 8){
             p7 = 0.313;
-        else if (num7 <= 12)
+            w7 = 44.211;
+        }
+        else if (num7 <= 12){
             p7 = 0.146;
-        else
+            w7 = 54.167;
+        }
+        else{
             p7 = 0.034;
-
-        if (num8 <= 40)
+            w7 = 64.286;
+        }
+        if (num8 <= 40){
             p8 = 0.862;
-        else if (num8 <= 60)
+            w8 = 33.285;
+        }
+        else if (num8 <= 60){
             p8 = 0.131;
-        else
+            w8 = 48.611;
+        }
+        else{
             p8 = 0.007;
-
-        p = p1 * p2 * p3 * p4 * p5 * p6 *p7*p8* 0.359;
+            w8 = 100;
+        }
+        p = p1 * p2 * p3 * p4 * p5 * p6 *p7*p8* 0.359*w1*w2*w3*w4*w5*w6*w7*w8;
         return p;
     }
     private static double no_probability(double num1, double num2, double num3, double num4, double num5, double num6, double num7, double num8)
     {
         double p = 0;
         double p1, p2, p3, p4, p5, p6,p7,p8;
-        if (num1 <= 50)
+        double w1,w2,w3,w4,w5,w6,w7,w8;
+        if (num1 <= 50){
             p1 = 0.008;
-        else if (num1 <= 100)
+            w1 = 66.667;}
+        else if (num1 <= 100){
             p1 = 0.38;
-        else if (num1 <= 150)
+            w1 = 91.346;}
+        else if (num1 <= 150){
             p1 = 0.542;
-        else
+            w1 = 65.459;}
+        else{
             p1 = 0.07;
-
-        if (num2 <= 60)
+            w1 = 25;
+        }
+        if (num2 <= 60){
             p2 = 0.238;
-        else if (num2 <= 80)
-            p2 = 0.586;
-        else if (num2 <= 100)
-            p2 = 0.166;
-        else
+            w2 = 75.316;}
+        else if (num2 <= 80){
+        	w2 = 65.843;
+            p2 = 0.586;}
+        else if (num2 <= 100){
+        	w2 = 54.605;
+            p2 = 0.166;}
+        else{
+        	w2 = 38.462;
             p2 = 0.01;
-
-        if (num3 <= 100)
-            p3 = 0.736;
-        else if (num3 <= 200)
-            p3 = 0.186;
-        else if (num3 <= 300)
-            p3 = 0.046;
-        else
+        }
+        if (num3 <= 100){
+        	w3 = 70.1;
+            p3 = 0.736;}
+        else if (num3 <= 200){
+        	w3 = 58.9;
+            p3 = 0.186;}
+        else if (num3 <= 300){
+        	w3 = 47.9;
+            p3 = 0.046;}
+        else{
             p3 = 0.032;
-
-        if (num4 < 30)
-            p4 = 0.5;
-        else if (num4 < 40)
+            w3 = 43.2;
+        }
+        if (num4 < 30){
+        	w4 = 82.508;
+            p4 = 0.5;}
+        else if (num4 < 40){
             p4 = 0.416;
-        else
+            w4 = 56.369;}
+        else{
             p4 = 0.084;
-
-        if (num5 <= 0.5)
+            w4 = 43.75;
+        }
+        if (num5 <= 0.5){
             p5 = 0.698;
-        else if (num5 <= 1)
+            w5 = 71.079;}
+        else if (num5 <= 1){
             p5 = 0.258;
-        else if (num5 <= 2)
+            w5 = 57.08;}
+        else if (num5 <= 2){
             p5 = 0.042;
-        else
+            w5 = 44.681;}
+        else{
             p5 = 0.002;
-
-        if (num6 <= 20)
+            w5 = 25;
+        }
+        if (num6 <= 20){
             p6 = 0;
-        else if (num6 <= 30)
+            w6 = 0;}
+        else if (num6 <= 30){
             p6 = 0.654;
-        else if (num6 <= 40)
+            w6 = 78.4;}
+        else if (num6 <= 40){
             p6 = 0.162;
-        else if (num6 <= 50)
+            w6 = 51.6;}
+        else if (num6 <= 50){
             p6 = 0.098;
-        else if (num6 <= 60)
+            w6 = 43.4;}
+        else if (num6 <= 60){
             p6 = 0.046;
-        else
+            w6 = 42.6;}
+        else{
             p6 = 0.04;
+            w6 = 74.1;}
 
-        if (num7 <= 4)
+        if (num7 <= 4){
             p7 = 0.712;
-        else if (num7 <= 8)
+            w7 = 72.358;}
+        else if (num7 <= 8){
             p7 = 0.212;
-        else if (num7 <= 12)
+            w7 = 55.789;}
+        else if (num7 <= 12){
             p7 = 0.066;
-        else
+            w7 = 45.833;}
+        else{
             p7 = 0.01;
-
-        if (num8 <= 40)
-            p8 = 0.926;
-        else if (num8 <= 60)
+            w7 = 35.714;}
+        if (num8 <= 40){
+        	w8 = 66.715;
+            p8 = 0.926;}
+        else if (num8 <= 60){
             p8 = 0.074;
-        else
+            w8 = 51.389;
+        }
+        else{
             p8 = 0.0;
-        p = p1 * p2 * p3 * p4 * p5 * p6 *p7*p8*0.651;
+            w8 = 0;}
+        p = p1 * p2 * p3 * p4 * p5 * p6 *p7*p8*0.651*w1*w2*w3*w4*w5*w6*w7*w8;
         return p;
     }
 	public static void test (int test_number)throws NumberFormatException, IOException {		
 		CsvReader reader = new CsvReader("PID_dataset _test .csv",',',Charset.forName("GBK"));
 		double p1,p2,n1,n2,n3,n4,n5,n6,n7,n8;
 		int res,predict;
+		int y_n = 0,n_y = 0;
 		int correct = 0,incorrect=0;
 		reader.readHeaders();
 		while(reader.readRecord()&&test_number!=0){
@@ -184,19 +276,27 @@ public class PID_Dataset_Test {
 			else {
 				predict = 0;
 			}
+			System.out.println("p1:"+p1+"   p2:"+p2+"   Predict:"+predict);
 			res = Integer.parseInt(reader.get("col9"));
 			if(predict == res)
 				correct++;
-			else 
+			else {
 				incorrect++;
+				if(res==1)
+					y_n++;
+				else {
+					n_y++;
+				}
+			}
 			test_number--;
 		}
-		System.out.println(correct);
-		System.out.println(incorrect);
+		System.out.println("Correct:"+correct);
+		System.out.println("Incorrect:"+incorrect);
 		//System.out.println(correct+incorrect);  
 		BigDecimal cor = new BigDecimal(correct);
 		BigDecimal total = new BigDecimal(correct+incorrect);
-		System.out.println(cor.divide(total));
+		System.out.println("YesToNo:"+y_n+"   NoToYes:"+n_y);
+		//System.out.println(cor.divide(total));
 	}
 
 public static void main(String[] args) {	
@@ -210,5 +310,6 @@ public static void main(String[] args) {
 		e.printStackTrace();
 	}
 }
+
 }
 
